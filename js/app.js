544d9187f0dd9ccd2913244da07ef24dc216b699
Fernando Stadler <fernandostadler2@gmail.com>
2022-05-28 21:48:57 -0300

    adicionado o Canvas para os erros

--- js/app.js
+++ js/app.js
@@ -46,7 +46,7 @@ function escolheLetra(letra) {
         var button = document.getElementById(letra);
         button.setAttribute('class', 'errada');
         button.removeAttribute('onclick');
-        
+        hangman++;
         chance--;
     }
     
@@ -58,24 +58,44 @@ function escolheLetra(letra) {
     if (hits === words.length) {
         alert("Ganhou nessa porra")
     }
+
+    if(hangman>0){
+        circle(200, 130, 25, "blue");
+        
+    }
+    if(hangman>1){
+        draw(200, 154, 1, 100, "blue");
+        
+    }
+    if(hangman>2){
+        line(150, 200, 200, 160, "blue");
+        
+    }
+    if(hangman>3){
+        line(250, 200, 200, 160, "blue");
+        
+    }
+    if(hangman>4){
+        line(200, 250, 250, 290, "blue");
+        
+    }
+    if(hangman>5){
+        line(200, 250, 150, 290, "blue");
+        
+    }
 }
+
 
 
 tip.innerHTML = `dica: ${clues}`;
 
-draw(3, 5, 4, 350, "blue");
 draw(3, 5, 200, 4, "blue");
 draw(200, 5, 3, 100, "blue");
+draw(3, 5, 4, 350, "blue");
 // head
-circle(200, 130, 25, "blue");
-// body
-draw(200, 154, 1, 100, "blue");
-// left arm
-line(150, 200, 200, 160, "blue");
+// // body
+// // left arm
 // right arm
-line(250, 200, 200, 160, "blue");
 // right leg
-line(200, 250, 250, 290, "blue");
 // left leg
-line(200, 250, 150, 290, "blue");
 
